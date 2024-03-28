9803145242e21251c8b9f645bdea48466d08f0ad
saktiporn <suktipond.deemak@gmail.com>
2024-03-28 16:10:49 +0700

    add dependencies

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-//Support[ETH]
-function geneateWallet(type='eth'){
+var ethers = require('ethers');  
+var crypto = require('crypto'); 
+//Support[ETH,BTC]
+//console.log('init');
+const geneateWallet = (type='eth') => {
     var privateKey="";
     var publicKey=""; 
     switch (type){
-        case 'eth':
-            var ethers = require('ethers');  
-            var crypto = require('crypto'); 
+        case 'eth': 
             var id = crypto.randomBytes(32).toString('hex');
             privateKey = "0x"+id; 
             var wallet = new ethers.Wallet(privateKey);  
             publicKey=wallet.address;  
-        break;
+        case 'btc':
+            break;
+        break; 
     } 
     console.log("PublicKey:"+publicKey);
     console.log("PrivateKey:"+privateKey);
     return [publicKey,privateKey];
 }
-module.exports=geneateWallet;
+//var[a,b]=geneateWallet();
+//console.log(a);
+//console.log(b);
+module.exports={geneateWallet};
