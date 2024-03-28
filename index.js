81d99e7796aaef84550ad36d3793edb4e942db04
saktiporn <suktipond.deemak@gmail.com>
2024-03-28 14:29:07 +0700

    init packge to github

---
+++ index.js
@@ -0,0 +1,15 @@
+class MultiWallet
+{
+    ethWallet=function(){
+
+    }
+}
+//Support[ETH]
+function geneateWallet(type='eth',totalWallet=1){
+    var w=new MultiWallet();
+    w.ethWallet();
+    var privateKey="";
+    var publicKey=""; 
+    return new Array();
+}
+modules.export=geneateWallet;
